769ff0687c02bba8ff7845416235970972b6cef9
Julian Gruber <julian@juliangruber.com>
2023-03-20 14:02:24 +0100

    docs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const min = 1024
 const max = 65535
 
 const namedPort = (str) => {
+  // TODO: sha512 isn't big enough to cover the full port range
   const hash = crypto.createHash('sha512').update(str).digest()
   const n = hash.reduce((a, b) => a + b, 0)
   return min + n % (max - min)
